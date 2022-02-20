634d520c1738db7f0d094556fb484b70ffe156e9
Yoshika <phpbbtw@gmail.com>
2022-02-20 22:31:46 +0800

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,28 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+Home
+<p ref="p">My name is {{ name }} and my age is {{ age }}</p>
+<button @click="handleClick">Click me</button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import { ref } from '@vue/reactivity'
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  setup() {
+    // const p = ref(null)
+
+
+    const name = ref('mario')
+    const age = ref(18)
+
+    const handleClick = () => {
+      name.value = 'luigi'
+      age.value = 20
+    }
+
+    return { name, age, handleClick }
   }
 }
 </script>
